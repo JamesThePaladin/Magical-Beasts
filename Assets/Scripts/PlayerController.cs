1d94c79ac5526923e1e8a44b358081e007a4ed6c
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-10-12 18:10:40 -0700

    Player & Script Update
    Added player gameObject complete with code for movement and controlling. Imported scripts for camera management, interactables, and respective pawns + controllers.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Controller
+{
+    //store movement data
+    [HideInInspector]
+    public Vector2 movement; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set movement x and y values to appropriate axis
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        pawn.Move(movement);
+    }
+}
